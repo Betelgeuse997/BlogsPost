a454830277a2fad2d4506bf7f3ee39140521b943
Betelgeuse997 <vishnu.swaroop63@gmail.com>
2024-10-02 16:03:00 +0530

    Updated blogController.js and blogsRoute.js

---
+++ backend/controller/blogController.js
@@ -0,0 +1,198 @@
+const blogModel = require("../models/blogModel.js");
+const userModel = require("../models/userModel.js")
+
+const blogs = async (req,res) => {
+    try {
+        const currentUser = await userModel.findById(req.user._id);
+        const followingIds = currentUser.following.map(user => user.user);
+        const blogs = await blogModel.find({
+            $or: [
+                { 'userName.user': req.user._id },
+                { 'userName.user': { $in: followingIds } }
+            ]
+        });
+        return res.status(200).json({
+            count: blogs.length,
+            data: blogs
+        });
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const userBlogs = async (req,res) => {
+    try {const { id } = req.params;
+        const blogs = await blogModel.find({'userName.user': id });
+        return res.status(200).json({
+            count: blogs.length,
+            data: blogs
+        });
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const getBlogsId = async (req,res) => {
+    try {
+        const { id } = req.params;
+        const blog = await blogModel.findById(id);
+        return res.status(200).json(blog);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const putBlogsId = async (req,res) => {
+    try {
+        const { id } = req.params;
+        const blogresult = await blogModel.findById(id)
+
+        if (
+            !req.body.title || !req.body.story
+        ) {
+            return res.status(400).send({
+                message: 'All fields required',
+            });
+        }
+
+        const { _id: userId } = req.user;
+    
+        if (userId.toString() === blogresult.userName.user.toString())
+        {
+            const result = await blogModel.findByIdAndUpdate(id, req.body);
+
+            if (!result) {
+                return res.status(404).json({ message: 'Blog not found'});
+            }
+
+            return res.status(200).send({ message: 'Blog updated successfully'});
+
+        }
+
+        else {
+            return res.status(403).send({ message: 'Forbidden to edit'});
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const deleteBlog = async (req,res) => {
+    try {
+        const { id } = req.params;
+        const blogresult = await blogModel.findById(id, req.body)
+
+        if (!blogresult) {
+            return res.status(404).json({ message: 'Blog not found' });
+          }
+
+        const { _id: userId } = req.user;
+
+        if (userId.toString() === blogresult.userName.user.toString())
+        {   
+            const result = await blogModel.findByIdAndDelete(id, req.body);
+
+            if (!result) {
+                return res.status(404).json({ message: 'Blog not found'});
+            }
+
+            return res.status(200).send({ message: 'Blog deleted'});
+        }
+        else {
+            return res.status(403).send({ message: 'Forbidden to delete'});
+        }
+        
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const postBlog = async (req, res) => {
+    try {
+        if (
+            !req.body.title || !req.body.story
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields'
+            });
+        }
+        const newBlog = {
+            title: req.body.title,
+            story: req.body.story,
+            likes: [],
+            userName: { 
+                user: req.user._id,
+                name: req.user.name, 
+            },
+        };
+        const blog = await blogModel.create(newBlog);
+        return res.status(201).send(blog);
+    }
+    catch (error) {
+        console.log(error.message);
+        console.log(req.user)
+        res.status(500).send({ message: error.message});
+    }
+};
+
+const likeBlog = async (req,res) => {
+    try {
+        
+        const { id } = req.params;
+        const { _id: userId } = req.user;
+        const blog = await blogModel.findById(id);
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
+        }
+
+        // Check if the user has already liked the blog
+        if (blog.likes.includes(userId)) {
+            return res.status(400).json({ message: 'Blog already liked by the user' });
+        }
+
+        blog.likes.push(userId);
+        await blog.save();
+
+        return res.status(200).send({ message: 'Blog liked successfully'});
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message });
+    }
+};
+
+const unlikeBlog = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { _id: userId } = req.user;
+        const blog = await blogModel.findById(id);
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
+        }
+
+        // Check if the user has liked the blog
+        if (!blog.likes.includes(userId)) {
+            return res.status(400).json({ message: 'Blog not liked by the user' });
+        }
+
+        blog.likes = blog.likes.filter(likedUserId => {
+            return likedUserId.toString() !== userId.toString();
+          });
+        await blog.save();
+
+        return res.status(200).json({ message: 'Blog unliked successfully' });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+};
+
+module.exports = {blogs, userBlogs, getBlogsId, putBlogsId, deleteBlog, postBlog, likeBlog, unlikeBlog};
--- backend/routes/blogsRoute.js
+++ backend/routes/blogsRoute.js
@@ -1,202 +1,23 @@
 const express = require("express");
 const auth = require("../middleware/authMiddleware.js")
-const blogModel = require("../models/blogModel.js");
-const userModel = require("../models/userModel.js")
+const {blogs, userBlogs, getBlogsId, putBlogsId, deleteBlog, postBlog, likeBlog, unlikeBlog} = require("../controller/blogController.js");
 
 const router = express.Router();
 
-router.get('/blogs',auth, async (req,res) => {
-    try {
-        const currentUser = await userModel.findById(req.user._id);
-        const followingIds = currentUser.following.map(user => user.user);
-        const blogs = await blogModel.find({
-            $or: [
-                { 'userName.user': req.user._id },
-                { 'userName.user': { $in: followingIds } }
-            ]
-        });
-        return res.status(200).json({
-            count: blogs.length,
-            data: blogs
-        });
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
+router.get('/blogs',auth, blogs);
 
-router.get('/userblogs/:id',auth, async (req,res) => {
-    try {const { id } = req.params;
-        const blogs = await blogModel.find({'userName.user': id });
-        return res.status(200).json({
-            count: blogs.length,
-            data: blogs
-        });
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
+router.get('/userblogs/:id', auth, userBlogs);
 
-router.get('/blogs/:id', auth, async (req,res) => {
-    try {
-        const { id } = req.params;
-        const blog = await blogModel.findById(id);
-        return res.status(200).json(blog);
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
+router.get('/blogs/:id', auth, getBlogsId);
 
-router.put('/blogs/:id', auth, async (req,res) => {
-    try {
-        const { id } = req.params;
-        const blogresult = await blogModel.findById(id)
+router.put('/blogs/:id', auth, putBlogsId);
 
-        if (
-            !req.body.title || !req.body.story
-        ) {
-            return res.status(400).send({
-                message: 'All fields required',
-            });
-        }
+router.delete('/blogs/:id', auth,  deleteBlog);
 
-        const { _id: userId } = req.user;
-    
-        if (userId.toString() === blogresult.userName.user.toString())
-        {
-            const result = await blogModel.findByIdAndUpdate(id, req.body);
+router.post('/blogs/', auth, postBlog);
 
-            if (!result) {
-                return res.status(404).json({ message: 'Blog not found'});
-            }
+router.put('/likeblog/:id', auth, likeBlog);
 
-            return res.status(200).send({ message: 'Blog updated successfully'});
-
-        }
-
-        else {
-            return res.status(403).send({ message: 'Forbidden to edit'});
-        }
-        
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
-
-router.delete('/blogs/:id', auth,  async (req,res) => {
-    try {
-        const { id } = req.params;
-        const blogresult = await blogModel.findById(id, req.body)
-
-        if (!blogresult) {
-            return res.status(404).json({ message: 'Blog not found' });
-          }
-
-        const { _id: userId } = req.user;
-
-        if (userId.toString() === blogresult.userName.user.toString())
-        {   
-            const result = await blogModel.findByIdAndDelete(id, req.body);
-
-            if (!result) {
-                return res.status(404).json({ message: 'Blog not found'});
-            }
-
-            return res.status(200).send({ message: 'Blog deleted'});
-        }
-        else {
-            return res.status(403).send({ message: 'Forbidden to delete'});
-        }
-        
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
-
-router.post('/blogs/', auth, async (req, res) => {
-    try {
-        if (
-            !req.body.title || !req.body.story
-        ) {
-            return res.status(400).send({
-                message: 'Send all required fields'
-            });
-        }
-        const newBlog = {
-            title: req.body.title,
-            story: req.body.story,
-            likes: [],
-            userName: { 
-                user: req.user._id,
-                name: req.user.name, 
-            },
-        };
-        const blog = await blogModel.create(newBlog);
-        return res.status(201).send(blog);
-    }
-    catch (error) {
-        console.log(error.message);
-        console.log(req.user)
-        res.status(500).send({ message: error.message});
-    }
-});
-
-router.put('/likeblog/:id', auth, async (req,res) => {
-    try {
-        
-        const { id } = req.params;
-        const { _id: userId } = req.user;
-        const blog = await blogModel.findById(id);
-
-        if (!blog) {
-            return res.status(404).json({ message: 'Blog not found' });
-        }
-
-        // Check if the user has already liked the blog
-        if (blog.likes.includes(userId)) {
-            return res.status(400).json({ message: 'Blog already liked by the user' });
-        }
-
-        blog.likes.push(userId);
-        await blog.save();
-
-        return res.status(200).send({ message: 'Blog liked successfully'});
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: error.message });
-    }
-});
-
-router.put('/unlikeblog/:id', auth, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { _id: userId } = req.user;
-        const blog = await blogModel.findById(id);
-
-        if (!blog) {
-            return res.status(404).json({ message: 'Blog not found' });
-        }
-
-        // Check if the user has liked the blog
-        if (!blog.likes.includes(userId)) {
-            return res.status(400).json({ message: 'Blog not liked by the user' });
-        }
-
-        blog.likes = blog.likes.filter(likedUserId => {
-            return likedUserId.toString() !== userId.toString();
-          });
-        await blog.save();
-
-        return res.status(200).json({ message: 'Blog unliked successfully' });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message });
-    }
-});
+router.put('/unlikeblog/:id', auth, unlikeBlog);
 
 module.exports = router;
